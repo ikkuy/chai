f4e979fe5a0c03c16b4057cd91e86417c61b4616
ikkuy <cjt199105@163.com>
2023-12-22 02:03:07 +0800

    引入druid，配置druid监控平台；连接mysql

--- src/main/java/com/cjt/WebApplication.java
+++ src/main/java/com/cjt/WebApplication.java
@@ -1,11 +1,59 @@
 package com.cjt;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class);
+    }
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DruidDataSource druidDataSource(){
+        return new DruidDataSource();
+    }
+
+    /**
+     * 配置监控服务器 http://localhost:8088/druid/index.html
+     * @return 返回监控注册的servlet对象
+     * @author chaijt
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        // 添加IP白名单
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        // 添加IP黑名单，当白名单和黑名单重复时，黑名单优先级更  高
+        servletRegistrationBean.addInitParameter("deny", "127.0.0.1");
+        // 添加控制台管理用户
+        servletRegistrationBean.addInitParameter("loginUsername", "chai");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        // 是否能够重置数据
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 配置服务过滤器
+     *
+     * @return 返回过滤器配置对象
+     */
+    @Bean
+    public FilterRegistrationBean statFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        // 添加过滤规则
+        filterRegistrationBean.addUrlPatterns("/*");
+        // 忽略过滤格式
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*,");
+        return filterRegistrationBean;
     }
 }
---
+++ src/main/java/com/cjt/test/TestController.java
@@ -0,0 +1,14 @@
+package com.cjt.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "server is running";
+    }
+}
